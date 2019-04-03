e83ec3970b22be7f6df0b4e9163645b0c807ef25
William Pei Yuan <williampeiyuan@gmail.com>
2019-04-03 11:44:44 -0400

    Updates 3.2

---
+++ client/src/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// Import statement to indicate that you need to bundle `./index.scss`
+import './index.scss';
+
+// Main component (will eventually use all the others)
+class MyFlixApplication extends Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
+}
+
+// Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render your app in the root DOM element
+ReactDOM.render(<MyFlixApplication />, container);
