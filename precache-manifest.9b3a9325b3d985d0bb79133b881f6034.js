88810d614c6dbc35f7da0668be2e571d8e98425e
William Pei Yuan <williampeiyuan@gmail.com>
2019-05-14 13:50:51 -0400

    Updates

---
+++ precache-manifest.9b3a9325b3d985d0bb79133b881f6034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d660366f05f662271cbdb41ec824335",
+    "url": "/myFlixApp/index.html"
+  },
+  {
+    "revision": "1bcdde3c0cbec1d82529",
+    "url": "/myFlixApp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4632189eff480a736f4b",
+    "url": "/myFlixApp/static/css/main.3a577319.chunk.css"
+  },
+  {
+    "revision": "1bcdde3c0cbec1d82529",
+    "url": "/myFlixApp/static/js/2.48cacdc8.chunk.js"
+  },
+  {
+    "revision": "4632189eff480a736f4b",
+    "url": "/myFlixApp/static/js/main.fde6ea62.chunk.js"
+  },
+  {
+    "revision": "eb9c53c8bc0dc0bff9c5",
+    "url": "/myFlixApp/static/js/runtime~main.342561a4.js"
+  }
+]);
